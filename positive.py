1eba527c9af24687fc5fa097129ac69673b71ebd
210manish <102983980+210manish@users.noreply.github.com>
2022-04-25 21:34:03 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,12 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+new_li1=[]
+new_li2=[]
+for i in list1:
+	if i>0:
+		new_li1.append(i)
+for i in list2:
+	if i>0:
+		new_li2.append(i)
+print(new_li1)
+print(new_li2)
